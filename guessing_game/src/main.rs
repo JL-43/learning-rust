77e9796dbe4b948cd96286baa7725631a7f8c6e7
JL <joseluisreyes43@gmail.com>
2023-01-08 10:03:11 +0800

    completed guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,39 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess: ");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    loop{    
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        // :: -> call the new() static method from the String class
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        // old => crashes when input is not a number
+        // let guess: u32 = guess.trim().parse()
+        //     .expect("Please type a number!");
+
+        // new => resets the game when input is not a number
+        // switched from expect to match. Match returns Ok and Err for Good and Bad route respectively
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
---
+++ guessing_game/target/debug/deps/guessing_game-85bdf37a53db9c3c.d
@@ -0,0 +1,5 @@
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/guessing_game-85bdf37a53db9c3c: src/main.rs
+
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/guessing_game-85bdf37a53db9c3c.d: src/main.rs
+
+src/main.rs:
