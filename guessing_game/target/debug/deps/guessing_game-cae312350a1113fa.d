2bbd940bc5d8dc2b4e4667be15081f33601234ff
JL <joseluisreyes43@gmail.com>
2023-01-08 09:08:26 +0800

    created gitignore file

---
+++ guessing_game/target/debug/deps/guessing_game-cae312350a1113fa.d
@@ -0,0 +1,5 @@
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/guessing_game-cae312350a1113fa: src/main.rs
+
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/guessing_game-cae312350a1113fa.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/rand-cf29accecc97633a.d
@@ -0,0 +1,9 @@
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/rand-cf29accecc97633a.rmeta: /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/librand-cf29accecc97633a.rlib: /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/Users/jl/Documents/Development/rust/guessing_game/target/debug/deps/rand-cf29accecc97633a.d: /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
+/Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
+/Users/jl/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
