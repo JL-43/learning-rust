3675350dd6111e93b07be99ca1eeff81be4da4ed
JL <joseluisreyes43@gmail.com>
2023-01-21 18:29:44 +0800

    added enums patternmatching section

--- enums_patternsmatching/src/main.rs
+++ enums_patternsmatching/src/main.rs
@@ -1,5 +1,19 @@
+fn route(ip_kind:IpAddrKind){
+    
+}
+
 fn main() {
 
     //enums keep values between certain options
+
+    // in our example, we will create an enum for IP addressed (only two possible kinds: IPv4 and IPv6)
+
+    enum IpAddrKind{
+        V4,
+        V6
+    }
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 
 }
